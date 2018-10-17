456bba127c3c34faaf4e580e84c1aaeb3cdca5ba
gunthecl <33225856+gunthecl@users.noreply.github.com>
2018-10-17 17:41:02 +0200

    Updated prosumahe model

---
+++ Code/Prosumage_Opt/Prosumage Module/225a/gamsnext.cmd
@@ -0,0 +1,2 @@
+@echo off
+gmsgennt.cmd "C:\Users\cguenther\Documents\GitHub\prosumage\Code\Prosumage_Opt\Prosumage Module\225a\" "C:\Users\cguenther\Documents\GitHub\prosumage\Code\Prosumage_Opt\Prosumage Module\" %1 "C:\Users\cguenther\Documents\GitHub\prosumage\Code\Prosumage_Opt\Prosumage Module\225a\gamscntr.dat" "C:\GAMS\win64\24.2\" "CPLEX"
