7d8d142627ff8017964cfdcb9cd8015b1a4f2522
gunthecl <33225856+gunthecl@users.noreply.github.com>
2018-10-19 11:45:03 +0200

    Description on 24h dual added

--- Code/Prosumage_Opt/Prosumage Module/225a/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\Lenovo\Documents\GitHub\prosumage\Code\Prosumage_Opt\Prosumage Module\225a\" "C:\Users\Lenovo\Documents\GitHub\prosumage\Code\Prosumage_Opt\Prosumage Module\" %1 "C:\Users\Lenovo\Documents\GitHub\prosumage\Code\Prosumage_Opt\Prosumage Module\225a\gamscntr.dat" "C:\GAMS\win64\24.7\" "PATH"
