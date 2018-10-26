67f4727869a6ff1e3fa9aebc5c8e3f09d04408ad
gunthecl <33225856+gunthecl@users.noreply.github.com>
2018-10-26 15:32:05 +0200

    Added batch

---
+++ Code/Source_Code/Modelle/MORITS/quarter1/225a/gamsnext.cmd
@@ -0,0 +1,2 @@
+@echo off
+gmsgennt.cmd "C:\Users\cguenther\Documents\GitHub\prosumage\Code\Source_Code\Modelle\MORITS\quarter1\225a\" "C:\Users\cguenther\Documents\GitHub\prosumage\Code\Source_Code\Modelle\MORITS\quarter1\" %1 "C:\Users\cguenther\Documents\GitHub\prosumage\Code\Source_Code\Modelle\MORITS\quarter1\225a\gamscntr.dat" "C:\GAMS\win64\24.2\" "PATH"
