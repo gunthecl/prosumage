801256975b8dc36018d75d94f8be2b4619ef6f9e
gunthecl <33225856+gunthecl@users.noreply.github.com>
2018-12-03 08:10:09 +0100

    Fixed mcp model
    added energy balance duals to prosumage household

--- Code/Prosumage_Opt/Dieter_Module/Modified_MCP/225a/gamsnext.cmd
+++ Code/Prosumage_Opt/Dieter_Module/Modified_MCP/225a/gamsnext.cmd
@@ -1,4 +1,2 @@
 @echo off
-echo Error: GAMSCMEX did not write a next script.
-
-exit 6
+gmsgennt.cmd "C:\Users\Lenovo\Documents\GitHub\prosumage\Code\Prosumage_Opt\Dieter_Module\Modified_MCP\225a\" "C:\Users\Lenovo\Documents\GitHub\prosumage\Code\Prosumage_Opt\Dieter_Module\Modified_MCP\" %1 "C:\Users\Lenovo\Documents\GitHub\prosumage\Code\Prosumage_Opt\Dieter_Module\Modified_MCP\225a\gamscntr.dat" "C:\GAMS\win64\25.1\" "PATH"
