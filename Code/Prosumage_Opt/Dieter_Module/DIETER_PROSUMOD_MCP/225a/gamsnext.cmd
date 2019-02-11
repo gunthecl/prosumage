41fd44a18b5f5990ff88c8c9ca1a47cb073473a0
gunthecl <33225856+gunthecl@users.noreply.github.com>
2019-02-11 11:53:36 +0100

    delet unnecessary folders

--- Code/Prosumage_Opt/Dieter_Module/DIETER_PROSUMOD_MCP/225a/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\Lenovo\Documents\GitHub\prosumage\Code\Prosumage_Opt\Dieter_Module\DIETER_PROSUMOD_MCP\225a\" "C:\Users\Lenovo\Documents\GitHub\prosumage\Code\Prosumage_Opt\Dieter_Module\DIETER_PROSUMOD_MCP\" %1 "C:\Users\Lenovo\Documents\GitHub\prosumage\Code\Prosumage_Opt\Dieter_Module\DIETER_PROSUMOD_MCP\225a\gamscntr.dat" "C:\GAMS\win64\24.7\" "PATH"
